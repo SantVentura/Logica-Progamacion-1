4311618151b59ae8f8f178b5f884c12b45caaf49
SantVent <gabrielventura920@gmail.com>
2023-02-09 00:19:35 -0500

    actualizacion de validaciones

--- reto.js
+++ reto.js
@@ -9,12 +9,48 @@ let resultado = [num1,num2,num3];
 if (resultado[0] === resultado[1] || resultado[1] === resultado[2] ||resultado[2] === resultado[0]) {
     console.log('Si existen numeros iguales');
 } else {
-    console.log('No existen numeros iguales')
+    console.log('No existen numeros iguales');
 }
-    
 
+//mayor de los tres numeros 
+
+if (resultado[0] > resultado[1] && resultado[0] > resultado[2]) {
+    console.log(`el valor ${resultado[0]} es el mayor de los tres numeros`);
+} else {
+    
+}if (resultado[1] > resultado[0] && resultado[1] > resultado[2]) {
+    console.log(`el valor ${resultado[1]} es el mayor de los tres numeros`);
+} else {
+    
+}if (resultado[2] > resultado[0] && resultado[2] > resultado[1]) {
+    console.log(`el valor ${resultado[2]} es el mayor de los tres numeros`);
+} else {
+    
+}
+
+//menor de los tres numeros
+if (resultado[0] < resultado[1] && resultado[0] < resultado[2]) {
+    console.log(`el valor ${resultado[0]} es el menor de los tres numeros`);
+} else {
+    
+}if (resultado[1] < resultado[0] && resultado[1] < resultado[2]) {
+    console.log(`el valor ${resultado[1]} es el menor de los tres numeros`)
+} else {
+    
+}if (resultado[2] < resultado[0] && resultado[2] < resultado[1]) {
+    console.log(`el valor ${resultado[2]} es el menor de los tres numeros`)
+} else {
+    
+}
+
+//numero del medio 
+if (resultado[0]<resultado[1] && resultado[2]>resultado[1]) {
+    console.log(`el valor ${resultado[1]} es el numero del medio`)
+} else
+    
 console.log(resultado.sort( (a,b) => a - b));
-console.log(resultado.reverse())
+
+
 
 
 
